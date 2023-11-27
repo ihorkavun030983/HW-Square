cb05d92c7a9cf474a7c027afdf3ea1798afaa0b1
ihorkavun030983 <128299534+ihorkavun030983@users.noreply.github.com>
2023-11-27 13:19:23 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,23 +2,46 @@ const square = document.getElementById('square');
 const body = document.body;
 const bodyWidth = body.clientWidth;
 const squareWidth = square.clientWidth;
-let position = 0;
+let positionSquare = 0;
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'ArrowLeft') {
-    if (position > 0) {
-      position -= 10;
+    if (positionSquare > 0) {
+      positionSquare -= 10;
       square.style.left = parseInt(square.style.left) - 10 + 'px';
     }
-  
   } else if (event.key === 'ArrowRight') {
-
-    if (position < bodyWidth - squareWidth) {
-      position += 10;
+    if (positionSquare < bodyWidth - squareWidth) {
+      positionSquare += 10;
       square.style.left = parseInt(square.style.left) + 10 + 'px';
     }
   }
 });
+
+const bumperSquare = () => {
+  if (positionSquare + squareWidth >= bodyWidth ) {
+    positionSquare = bodyWidth - squareWidth - 20;
+    setTimeout(() => {
+      square.innerHTML = 'БЕМС';
+      setTimeout(() => {
+        square.innerHTML = '';
+      }, 2000);
+    }, 0);
+
+  } else if (positionSquare <= 0) {
+    positionSquare = 20;
+    setTimeout(() => {
+      square.innerHTML = 'БЕМС';
+      setTimeout(() => {
+        square.innerHTML = '';
+      }, 2000);
+    }, 0);
+    
+  }
+  square.style.left = positionSquare + 'px';
+}
+
+setInterval(bumperSquare);
 
 let positionX = 0;
 let positionY = 0;
@@ -30,39 +53,12 @@ document.addEventListener('keydown', (event) => {
     setTimeout(() => {
       positionY += 10;
       square.style.transform = `translate(${positionX}px, ${positionY}px)`;
+      square.style.transition = '0.5s';
     }, 200);
   } else if (event.ctrlKey) {
     square.style.height = square.offsetHeight * 0.6 + 'px';
     square.style.width = square.offsetWidth * 1.25 + 'px';
+    square.style.transition = '0.5s';
   };
 }
 );
-
-
-const bumperSquare = () => {
-  if (position + squareWidth >= bodyWidth ) {
-    position = bodyWidth - squareWidth - 20;
-    setTimeout(() => {
-      square.innerHTML = "БЕМС";
-      setTimeout(() => {
-        square.innerHTML = "";
-      }, 2000);
-    }, 0);
-
-  } else if (position <= 0) {
-    position = 20;
-    setTimeout(() => {
-      square.innerHTML = "БЕМС";
-      setTimeout(() => {
-        square.innerHTML = "";
-      }, 2000);
-    }, 0);
-    
-  }
-  square.style.left = position + 'px';
-}
-
-setInterval(bumperSquare);
-
-
-
